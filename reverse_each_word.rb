563e07ed6c958e187672dd7e3e7a1abc3ac7b2a2
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 18:36:58 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+=begin (this method uses .each)
+def reverse_each_word(string)
+
+  word_array = string.split(" ")
+  reversed_words = []
+  
+  word_array.each do |word|
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
+end
+
+=end
+
+def reverse_each_word(string)
+  word_array = string.split(" ")
+  reversed_words = []
+  
+  word_array.collect do |word|
+    reversed_words << word.reverse
+  end
+  
+  reversed_words.join(" ")
+end
