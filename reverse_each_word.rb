b31619d6560001edc53d3468d427417683de327e
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 18:25:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   reversed_words = []
   
   word_array.each do |word|
-    reversed_string << word.reverse
+    reversed_words << word.reverse
   end
   reversed_words.join(" ")
 end
